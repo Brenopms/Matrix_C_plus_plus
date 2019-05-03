cd4624ebd452face63142a649e360c12f95bd369
Breno Silva <brenosilva1@gmail.com>
2019-05-02 21:49:02 -0300

    separate header file in a .cpp

--- tp1_top.cpp
+++ tp1_top.cpp
@@ -4,35 +4,39 @@ using namespace std;
 
 int main() {
 	Matriz Y;
-	Matriz X(3,1,1), A(3,3,2), C(3,3,1);
-	Matriz W = C;
-	Matriz Z(A);
-	
-	//int numeroLinhas = A.getRows();
+	Matriz X(3,3,1);
+	//Matriz A(3,3,2);
+	Matriz C(3,2,1);
+	//A(3,3,2), C(3,3,1);
+	// Matriz W = C;
+	// Matriz Z(A);
+
+	cout << X << endl;
+		//int numeroLinhas = A.getRows();
 	//int numeroColunas = A.getCols();
 	//Y.unit(); // inicializa Y com a matriz identidade
-	//A(2,1)=10; // altera o valor de uma posição de A
+	//A(2,1)=10; // altera o valor de uma posiï¿½ï¿½o de A
 	//Y.zeros(); // modifica todos os elementos de Y para o valor zero
 	//C=A+A; // Soma
 	//cout<< "C:\n"<< C<< endl;
-	//C-=A; // Subtração
+	//C-=A; // Subtraï¿½ï¿½o
 	//cout<< "C:\n"<< C<< endl;
-	//A=C-A; // Subtração
+	//A=C-A; // Subtraï¿½ï¿½o
 	//cout<< "A:\n"<< A<< endl;
 	//A+=A; // Soma
 	//cout<< "A:\n"<< A<< endl;
-  //A=~Z; // A é igual a transposta de C
+  //A=~Z; // A ï¿½ igual a transposta de C
   //cout<< "A:\n"<< A<< endl;
 	//X.ones(); // modifica todos os elementos de X com 1s
-	//X*=2; // multiplicação por uma constante
+	//X*=2; // multiplicaï¿½ï¿½o por uma constante
 	//cout<< "X:\n"<< X<< endl;
-	//C=A*X; // multiplicação de matrizes
+	//C=A*X; // multiplicaï¿½ï¿½o de matrizes
 	//cout<< "C:\n"<< C<< endl;
-	//C*=X; // multiplicação de matrizes
+	//C*=X; // multiplicaï¿½ï¿½o de matrizes
   //if (A == C) // verifica a igualdade entre A e C
 	//cout<<"1\n";
 	//if(X != Y) // verifica a desigualdade entre A e C
   //cout<<"2\n";
-	//cout<< "C:\n"<< C<< endl; // Impressão de matrizes
+	//cout<< "C:\n"<< C<< endl; // Impressï¿½o de matrizes
   //cin>>Y; // leitura de dados para dentro da matriz Y
 }
